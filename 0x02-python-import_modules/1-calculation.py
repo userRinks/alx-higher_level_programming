f832a1f2c609020c618638a2d8e7f1b55d96b212
MistaRinks <rinkanyakevin@gmail.com>
2023-09-08 04:58:21 +0300

    #3( +Flags): Import functions and print result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 
+"""
+This program imports calculation functions from calculator_1.py
+and prints the results of various mathematical operations.
+"""
+
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+def main():
+    a = 10
+    b = 5
 
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
 
-print("{:d} + {:d} = {:d}".format(a, b, result_add))
-print("{:d} - {:d} = {:d}".format(a, b, result_sub))
-print("{:d} * {:d} = {:d}".format(a, b, result_mul))
-print("{:d} / {:d} = {:d}".format(a, b, result_div))
+    print("{:d} + {:d} = {:d}".format(a, b, result_add))
+    print("{:d} - {:d} = {:d}".format(a, b, result_sub))
+    print("{:d} * {:d} = {:d}".format(a, b, result_mul))
+    print("{:d} / {:d} = {:d}".format(a, b, result_div))
+
+if __name__ == "__main__":
+    main()
