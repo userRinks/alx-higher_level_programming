eab6e3e136aeeb83c2e361a66c7be3a074e72f15
MistaRinks <rinkanyakevin@gmail.com>
2023-09-08 05:04:28 +0300

    #5( +Rearrange loop): Import functions and print result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,7 @@ and prints the results of various mathematical operations.
 
 from calculator_1 import add, sub, mul, div
 
-def main():
+if __name__ = "__main__":
     a = 10
     b = 5
 
@@ -15,6 +15,3 @@ def main():
     print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))
-
-if __name__ == "__main__":
-    main()
