d0aef4ebf39e5b4587d3ead415893e75e0333bbc
MistaRinks <rinkanyakevin@gmail.com>
2023-09-08 07:36:46 +0300

    #3 [Fix: Advanced] Manipulate Bytecode using python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
-    
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        
-        for i in range(4, 6):
-            c = add(c, i)
+        for n in range(4, 6):
+            c = add(c, n)
         return c
-
     else:
         return sub(a, b)
