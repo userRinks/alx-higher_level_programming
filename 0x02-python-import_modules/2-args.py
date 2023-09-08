fae51345a1a06ddcc10276c7354b9fcbb403002c
MistaRinks <rinkanyakevin@gmail.com>
2023-09-08 05:30:02 +0300

    Print number and list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    for i, arg in enumerate(args, start=1):
+        print(f"{i}: {arg}")
