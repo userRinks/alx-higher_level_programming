250fe728ffc62f48814dbd7babc8b4f71ad60c28
MistaRinks <rinkanyakevin@gmail.com>
2023-09-12 06:32:04 +0300

    [Task 13:] Header file with function prototypes

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,8 +20,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-void stack_push(listint_t **stack, int value);
-int stack_pop(listint_t **stack);
-
 
 #endif /* LISTS_H */
