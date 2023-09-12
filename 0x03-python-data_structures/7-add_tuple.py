95f5a8cb5dba2b6ac88f3a8b4882af00d2d8f0c8
MistaRinks <rinkanyakevin@gmail.com>
2023-09-12 03:01:08 +0300

    Add two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    padded_a = tuple_a + (0, 0)
+    padded_b = tuple_b + (0, 0)
+
+    result = (padded_a[0] + padded_b[0], padded_a[1] + padded_b[1])
+    return result
