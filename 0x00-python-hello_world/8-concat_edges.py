3b58a7f5572450996fbe2ca1eaf28ba994baa1f3
MistaRinks <rinkanyakevin@gmail.com>
2023-09-19 08:20:40 +0300

    [#2: Retry] Truncating and appending strings

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str[39:66] + " " + "with Python")
+str = (str[39:66] + " " + str.split()[-4] + " " + str.split()[0])
 print(str)
