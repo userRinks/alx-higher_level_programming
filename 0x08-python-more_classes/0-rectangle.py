863824dd7ff2f10e64ec8de2ab37d90027c60f39
userRinks <rinkanyakevin@gmail.com>
2023-11-28 18:31:27 +0300

    Define empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""Define a class Rectangle"""
 
 class Rectangle:
-    """Empty class that defines a rectangle."""
+    """Empty class that defines the rectangle properties."""
     pass
