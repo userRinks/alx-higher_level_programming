c43604e28e1fedca91ebb7cdc22d8db22d88e6bf
userRinks <rinkanyakevin@gmail.com>
2023-11-29 19:25:35 +0300

    [Advanced 101:] Use backtracking to find and print all possible solutions to the N Queens problem for the given value of N

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+N Queens problem solver
+"""
+
+import sys
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen at board[row][col]
+    """
+    for i in range(row):
+        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+            return False
+    return True
+
+def solve_nqueens(board, row, n):
+    """
+    Solve the N Queens problem using backtracking
+    """
+    if row == n:
+        print([[i, board[i]] for i in range(n)])
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
+
+def nqueens(n):
+    """
+    Main function to solve the N Queens problem
+    """
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
