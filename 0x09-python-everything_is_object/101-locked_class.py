ca99dd2291ac0beaf3c752f009f475aded49b785
userRinks <rinkanyakevin@gmail.com>
2023-11-29 20:43:08 +0300

    [+description Advanced 101:] Lock creation of specific attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""" Locked class """
+
 
 class LockedClass:
+    """ Lock creation of attributes, only allow specific """
     __slots__ = ['first_name']
+
+    def __init__(self):
+        """Initiate method"""
+        pass
