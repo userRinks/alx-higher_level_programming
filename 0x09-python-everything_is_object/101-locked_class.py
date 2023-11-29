41ca3cf5d61eadbc56e8bf527cce20846c8456d7
userRinks <rinkanyakevin@gmail.com>
2023-11-29 20:38:53 +0300

    [shorten Advanced 101:] Lock creation of specific attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,9 +2,3 @@
 
 class LockedClass:
     __slots__ = ['first_name']
-
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(
-                f"'LockedClass' object has no attribute '{name}'"
-            )
