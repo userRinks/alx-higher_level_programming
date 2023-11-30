2d949fbf3eba080854b2f1617bece0d902375371
userRinks <rinkanyakevin@gmail.com>
2023-11-30 16:12:03 +0300

    Add two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Adds two integers with Parameters:
+add_integer: Adds two integers with Parameters:
     a (int or float): The first parameter.
     b (int or float, optional): The second parameter. Defaults to 98.
 """
 
 
 def add_integer(a, b=98):
-    """
-    Returns - int: The sum of a and b.
+    """ Returns - int: The sum of a and b.
+		Args: a; b
     """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
