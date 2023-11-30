a59dbdf9b2efdcf375d5b90fb51c4e4fc3039d0b
userRinks <rinkanyakevin@gmail.com>
2023-11-30 21:01:47 +0300

    Print first and last name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Print My name is <first name> <last name> """
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints a formatted string stating the first and last name.
+
+    Parameters:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to empty string
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    Returns:
+        str: A formatted string indicating the full name.
+    """
+    try:
+        if not isinstance(first_name, str):
+            raise TypeError("first_name must be a string")
+        if not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
+        else:
+            return ("My name is {} {}".format(first_name, last_name))
+    except TypeError as e:
+        return str(e)
