d8a0586a71d320fac3e39be3abb49255d01c3dda
userRinks <rinkanyakevin@gmail.com>
2023-11-30 16:17:01 +0300

    Add two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,11 +7,12 @@ add_integer: Adds two integers with Parameters:
 
 
 def add_integer(a, b=98):
-    """ Returns - int: The sum of a and b.
-		Args: a; b
     """
-    if type(a) not in (int, float):
-        raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    Returns - int: The sum of a and b.
+    """
+
+    if isinstance(a, (int, float)) is False:
+        raise TypeError('a must be an integer')
+    if isinstance(b, (int, float)) is False:
+        raise TypeError('b must be an integer')
+    return (int(a) + int(b))
