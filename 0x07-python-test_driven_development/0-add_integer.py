8c220caf7aa95ee15a10f509fdc59d414a50f31d
userRinks <rinkanyakevin@gmail.com>
2023-11-30 17:07:19 +0300

    [My OG code]

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-   i donr hey
+add_integer: Adds two integers with Parameters:
+    a (int or float): The first parameter.
+    b (int or float, optional): The second parameter. Defaults to 98.
 """
 
 
 def add_integer(a, b=98):
-    """ add two integers or flots """
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return (a + b)
+    """
+    Returns - int: The sum of a and b.
+    """
+
+    if isinstance(a, (int, float)) is False:
+        raise TypeError('a must be an integer')
+    if isinstance(b, (int, float)) is False:
+        raise TypeError('b must be an integer')
+    return (int(a) + int(b))
