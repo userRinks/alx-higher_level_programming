35256c4ab5a7d07e64f8585967a78231d6882f01
userRinks <rinkanyakevin@gmail.com>
2023-12-01 00:42:00 +0300

    Pri~t text with 2 new lines after special characters to turn sentences into paragraphs

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,6 @@
 Prints a text with 2 new lines after each '.', '?' or `:` character
 """
 
-
 def text_indentation(text):
     """ Parameters:
         text (str): The input text.
@@ -11,32 +10,24 @@ def text_indentation(text):
         TypeError: If text is not a string.
     Returns: None
     """
-    # Check if text is a string
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # Initialize the index
     index = 0
 
-    # Skip leading spaces
     while index < len(text) and text[index] == ' ':
         index += 1
 
-    # Iterate through the text
     while index < len(text):
-        # Print the character
         print(text[index], end="")
 
-        # Check for newline or specified characters
-        if text[index] == "\n" or text[index] in ".?:":
+        if text[index].isspace() or text[index] in ".?:":
             if text[index] in ".?:":
                 print("\n")
             index += 1
 
-            # Skip trailing spaces
             while index < len(text) and text[index] == ' ':
                 index += 1
             continue
 
-        # Move to the next character
         index += 1
