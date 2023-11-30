819683d0fa4f2efde0ddc711795adb82fe68c975
userRinks <rinkanyakevin@gmail.com>
2023-11-30 19:19:38 +0300

    Divide all elements of a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
-"""
-Parameters:
-    matrix (list of lists): The matrix to be divided.
-    div (number): The divisor.
-Returns:
-    list of lists: A new matrix with elements rounded to 2 d.p.
-"""
+
+"""Matrix Division Function"""
 
 
 def matrix_divided(matrix, div):
+    """Divide all elements of a matrix.
+
+    Args:
+        matrix: A list of lists containing integers or floats.
+        div (int/float): The divisor.
+
+    Raises:
+        TypeError: Matrix contains non-numeric elements.
+        TypeError: Matrix contains rows of unequal sizes.
+        TypeError: div is not an integer or float.
+        ZeroDivisionError: div = 0.
+
+    Returns:
+        Matrix (list) representing the result of the division.
     """
-    Divides all elements of a matrix by a given divisor.
-    """
-    if (not isinstance(matrix, list) or
-            not all(isinstance(row, list) for row in matrix)):
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all(isinstance(e, (int, float))
+                    for e in [num for row in matrix for num in row])):
         raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
+            "matrix must be a matrix (list of lists) of "
+            "integers/floats"
         )
 
-    row_size = len(matrix[0])
-    if not all(len(row) == row_size for row in matrix):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
@@ -28,7 +37,4 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [
-        [round(element / div, 2) for element in row] for row in matrix
-    ]
-    return new_matrix
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
