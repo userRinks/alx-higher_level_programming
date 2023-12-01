6db89deb41adad4884499d8f8a3bdbcac2518864
userRinks <rinkanyakevin@gmail.com>
2023-12-01 21:39:19 +0300

    [Advanced 102:] Use C to print Python strings with Unicode characters

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -7,29 +7,26 @@
  */
 void print_python_string(PyObject *p)
 {
-	if (PyUnicode_Check(p))
+	PyASCIIObject *ascii_obj;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
-		wchar_t *wstr;
-		Py_ssize_t size;
-
-		printf("[.] string object info\n");
-
-		if (PyUnicode_IS_COMPACT_ASCII(p))
-			printf("  type: compact ascii\n");
-		else
-			printf("  type: compact unicode object\n");
-
-		size = PyUnicode_GET_LENGTH(p);
-		wstr = PyUnicode_AsWideCharString(p, NULL);
-
-		printf("  length: %zd\n", size);
-		printf("  value: %ls\n", wstr);
-
-		PyMem_Free(wstr);
+		printf("  [ERROR] Invalid String Object\n");
+		return;
 	}
+
+	ascii_obj = (PyASCIIObject *)p;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
 	else
-	{
-		fprintf(stderr, "[.] string object info\n");
-		fprintf(stderr, "  [ERROR] Invalid String Object\n");
-	}
+		printf("  type: compact unicode object\n");
+
+	printf("  length: %ld\n", ascii_obj->length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &ascii_obj->length));
 }
+
