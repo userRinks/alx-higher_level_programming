6d42d10c13fed38a580a4a8d70e6734d7a3d0a7a
userRinks <rinkanyakevin@gmail.com>
2023-12-06 19:07:56 +0300

    Enhance inherited Square class with custom description

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherited from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a Square instance."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description."""
+        return "[Square] {}/{}".format(
+            self._Rectangle__width, self._Rectangle__height
+        )
+
