0a7caf4141cfde3aea80a1cf446ab098874220ae
userRinks <rinkanyakevin@gmail.com>
2023-12-06 15:51:39 +0300

    Define a class `Student` with public instance attributes and a to_JSON serialization method

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module defining the Student class.
+"""
+
+
+class Student:
+    """
+    Student class with public instance attributes and a to_json method.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        # Get the dict representation of the object's __dict__ attribute
+        return self.__dict__
